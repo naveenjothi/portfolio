67c191c1954e0c87b5e4f1410240c9541be43839
naveenjothi <naveenjothi040@gmail.com>
2024-03-20 22:28:55 +0530

    updated layout and nav item

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,31 @@
 <template>
+  <header
+    className="pointer-events-none relative z-50 flex flex-none flex-col"
+    :style="{
+      height: 'var(--header-height)',
+      marginBottom: 'var(--header-mb)',
+    }"
+  >
+    <div class="top-0 order-last -mb-3 pt-3">
+      <div class="top-[var(--header-top,theme(spacing.6))] w-full">
+        <div class="relative flex gap-4">
+          <div class="flex flex-1 justify-end md:justify-center">
+            <nav>
+              <ul
+                class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+              >
+                <NavItem href="/about">About</NavItem>
+                <NavItem href="/articles">Articles</NavItem>
+                <NavItem href="/projects">Projects</NavItem>
+                <NavItem href="/speaking">Speaking</NavItem>
+                <NavItem href="/uses">Uses</NavItem>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
   <div class="relative">
     <NuxtPage />
   </div>
