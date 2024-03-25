c3733186955b5038f137d90ac71dcc387764d3fe
naveenjothi <naveenjothi040@gmail.com>
2024-03-26 00:42:26 +0530

    Added Changes

--- components/home/jumbotron.vue
+++ components/home/jumbotron.vue
@@ -1,19 +1,47 @@
 <template>
-  <section id="hero" class="jumbotron">
-    <div class="container">
-      <h1 class="hero-title load-hidden">
-        Hey, there !
-        <br />
-        I'm
-        <span class="text-color-main">Naveen Jothi</span>
+  <div class="container mx-auto px-4 bg-[#E6EFEA]">
+    <header class="flex justify-between items-center py-6">
+      <div>
+        <a href="/">
+          <img
+            src="../../assets/resources/nj-logo-bg-removed.png"
+            alt="Logo image"
+            class="mx-auto rounded-full w-24 h-24 object-cover"
+          />
+        </a>
+      </div>
+      <!-- Navigation -->
+      <nav>
+        <ul class="flex space-x-4">
+          <li>
+            <a
+              href="/contact"
+              class="text-white bg-[#629677] hover:bg-purple-700 px-4 py-2 rounded-full"
+              >Say Hello</a
+            >
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="text-center my-16">
+      <!-- Hero Section -->
+      <h1 class="text-5xl font-bold text-gray-900">
+        Software Engineer & Team Contributor
       </h1>
-
-      <p class="hero-content">
-        I'm a professional software engineer creating scalable architecture for
-        modern websites
+      <p class="text-xl text-gray-700 mt-4">
+        Crafting the Backbone of Modern Applications – A Journey with Naveen
+        Jothi
       </p>
-    </div>
-  </section>
+      <!-- Hero Image -->
+      <div class="mt-8">
+        <img
+          src="../../assets/resources/nj-avatar.svg"
+          alt="Your image"
+          class="mx-auto rounded-full w-48 h-48 object-cover"
+        />
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped lang="scss">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["./assets/scss/main.scss", "./assets/scss/hero.scss"],
+  css: ["./assets/scss/main.scss"],
   ssr: false,
   postcss: {
     plugins: {
